ac55d9b4acf6d029b36e3f1fefe4bb5e9026a127
Tom Thomas(duckLap) <tom.thomas09@gmail.com>
2019-06-17 09:34:21 +0530

    Added one working youBro sms file on Py3

--- readSMSAnd1.py
+++ readSMSAnd1.py
@@ -5,25 +5,22 @@ droid = androidhelper.Android()
 
 def test_message1():
   Allresult = droid.smsGetMessages(False) #returns all sms
-  print '\n ----' , len(Allresult[1]) , ' All sms!, Read+Unread'
-
+  # Allresult = [{u'body': u'The available credit limit is now Rs. 12,608.04.', u'status': u'-1', u'read': u'0', u'address': u'AD-CITIBK', u'date': u'1543932311088', u'_id': u'10668', u'type': u'1'}, 
+  #              {u'body': u'Dear Customer, get 5% discount', u'status': u'-1', u'read': u'0', u'address': u'MD-459159', u'date': u'1543928139311', u'_id': u'10666', u'type': u'1'}
+  #			    ]
   countCiti = 0 
-  for cnt, msgDict in enumerate(Allresult[1]):  # loop through all messages and print in Key::Value format
-	for sngMsg in msgDict:
-		if sngMsg.startswith('body'):  # Store the body part of the message.
-			textMsg = msgDict['body'] 
+  for cnt, sngMsgKeyDict in enumerate(Allresult[1]):  # loop through all messages and print in Key::Value format
+	if bool(re.search("(C|c)(I|i)(T|t)(I|i)(B|b)(K|k)", sngMsgKeyDict['address'])):  # using regular expr, checking if the Address name is CITIBK or citibk.
+		countCiti = countCiti + 1
 		
-		if sngMsg.startswith('date'):   # Convert the raw date(epoch time) part of the message.
-			dtReadable = datetime.datetime.fromtimestamp(int(msgDict['date'])//1000) 
+		if countCiti < 5:  # execute below statements if count is less than the given number
+			dtReadable = datetime.datetime.fromtimestamp(int(sngMsgKeyDict['date'])//1000) # Convert the raw date(epoch time) part of the message.
+			textMsg = sngMsgKeyDict['body'] # Store the body part of the message.
+			print sngMsgKeyDict['address'], " :: ", textMsg
+			print "date :: ", dtReadable 
 	
-		if sngMsg.startswith('address'):
-			if bool(re.search("(C|c)(I|i)(T|t)(I|i)(B|b)(K|k)", msgDict['address'])):  # using regular expr, checking if the Address name is CITIBK or citibk.
-				print(msgDict['address'], " :: ", textMsg)
-				print("date :: ", dtReadable)
-				countCiti = countCiti + 1
-				del textMsg, dtReadable
-	
-  print("\nTotal Citibk Msgs = ", countCiti)
+  #print "\nTotal Citibk Msgs = ", countCiti
+  print '\n ----' , len(Allresult[1]) , ' All sms!, Read+Unread'
 
 if __name__ == '__main__':
   test_message1()
