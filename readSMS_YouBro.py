2e082d55f059ca4c99d69f44b5c3cd97b4b38654
Tom Thomas(duckLap) <tom.thomas09@gmail.com>
2019-06-17 11:11:43 +0530

    YouBroadband SMS filtering to get the time elapsed between their service disruption

--- readSMS_YouBro.py
+++ readSMS_YouBro.py
@@ -8,28 +8,38 @@ def test_message1():
   Allresult = droid.smsGetMessages(False) #returns all sms
   print('\n ----' , len(Allresult[1]) , ' All sms!, Read+Unread')
 
-  countCiti = 0 
-  for cnt, msgDict in enumerate(Allresult[1]):  # loop through all messages and print in Key::Value format
-	   for sngMsg in msgDict:
-		    if sngMsg.startswith('body'):  # Store the body part of the message.
-		        textMsg = msgDict['body'] 
-			
-		    if sngMsg.startswith('date'):   # Store the raw date(epoch time) part of the message.
-		        dtReadable = datetime.datetime.fromtimestamp(int(msgDict['date'])//1000) 
-		
-		    if sngMsg.startswith('address'):
-		        #if bool(re.search('(C|c)(I|i)(T|t)(I|i)(B|b)(K|k)', msgDict['address'])):
-		            #print("date :: ", dtReadable)
-		            #countCiti = countCiti + 1
-		            #del textMsg, dtReadable
-		        
-		        if bool(re.search("(B|b)(r|R)(o|O)", msgDict['address'])):
-    		         textMsg = msgDict['body'] 
-    		         dtReadable = datetime.datetime.fromtimestamp(int(msgDict['date'])//1000) 
-    		         print(dtReadable, ':', msgDict['address'], " :: ", textMsg)
-    		         countCiti = countCiti + 1
-             
-  print("\nTotal YouBro Msgs = ", countCiti)
+  countFilterMsg = 0
+  startService = 0
+  stopService = 0
+  print("\nHalt_Minutes  Start", (' ')*18 , "Stop", (' ')*18)
+  print("\n", ('-')*18)
 
+  for cnt, msgDict in enumerate(Allresult[1]):  # loop through all messages and print in Key::Value format
+      for sngMsg in msgDict:
+          if sngMsg.startswith('address'):
+              if bool(re.search("(Y|y)(O|o)(U|u)(B|b)(r|R)(o|O)", msgDict['address'])):
+                  textMsg = msgDict['body'] 
+                  dtReadable = datetime.datetime.fromtimestamp(int(msgDict['date'])//1000)
+                  #print(dtReadable, ':', msgDict['address'], " :: ", textMsg)
+                  countFilterMsg = countFilterMsg + 1
+              
+                  if bool(re.search("We regret to inform", textMsg)):
+                      stopService = int(msgDict['date']) # capture epoch time for that event
+                      
+                  if bool(re.search("We are pleased to inform", textMsg)):
+                      startService = int(msgDict['date']) # capture epoch time for that event
+                  
+                  if startService != 0 and stopService != 0:
+                      epochDiffMillisec = startService - stopService
+                      strT = datetime.datetime.fromtimestamp(int(startService)//1000)
+                      endT = datetime.datetime.fromtimestamp(int(stopService)//1000)
+                      inMinutes = int(epochDiffMillisec/(60*1000))
+                      print(inMinutes, strT, endT)
+                      startService = 0
+                      stopService = 0
+
+             
+  print("\nTotal YouBro Msgs = ", countFilterMsg)
+  
 if __name__ == '__main__':
   test_message1()
